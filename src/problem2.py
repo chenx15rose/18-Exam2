7ddcdc2b8c46dd144b7e2f0e782ef241bba87a7f
chenx15 <chenx15@rose-hulman.edu>
2018-10-09 17:19:33 -0400

    problem 1&2 completed

--- src/problem2.py
+++ src/problem2.py
@@ -2,8 +2,8 @@
 Exam 2, problem 2.
 
 Authors: Dave Fisher, David Mutchler, Matt Boutell, their colleagues,
-         and PUT_YOUR_NAME_HERE.  October 2018.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Harry Chen (Xiaolong).  October 2018.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import math
 import time
@@ -109,8 +109,8 @@ def main():
     print('Un-comment the calls in MAIN one by one')
     print(' to run the testing code as you complete the TODOs.')
 
-    # run_test_problem2a()
-    # run_test_problem2b()
+    run_test_problem2a()
+    run_test_problem2b()
 
 
 def run_test_problem2a():
@@ -195,8 +195,13 @@ def problem2a(triangle):
       :type triangle: Triangle
       :rtype: Triangle
     """
+    a = 2 * (triangle.a)
+    b = 2 * (triangle.b)
+    c = 2 * (triangle.c)
+    return Triangle(a,b,c)
+
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
 
@@ -353,8 +358,16 @@ def problem2b(triangles):
       :type triangles: list | tuple of Triangle
       :rtype: int | float
     """
+    sum = 0
+    if triangles ==[]:
+        return 0
+    else:
+        for k in range(len(triangles)):
+            sum += triangles[k].get_area()
+    return sum
+
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #          Tests have been written for you (above).
     # IMPORTANT: See the HINT just before the DEF of this function.
     # -------------------------------------------------------------------------
